6ea904c65165800a0e7d4f9c63f553200f814b6a
Patrick Elliott <patrick.d.elliott@gmail.com>
2018-05-09 15:35:47 +0200

    removed some cruft

--- intensionality/Intensionality.hs
+++ intensionality/Intensionality.hs
@@ -17,10 +17,6 @@ import CharacteristicFunctions
 
 -- Binding theory not very promising. How to derive locality?
 
--- Can we type-lift Each?
-
--- Need a function from ((a -> T) -> T) -> ((a -> (S -> T)) -> (S -> T))
-
 each ∷ (Ent → T) → (Ent → T) → T
 each p q = all (`elem` (toGraph1 q)) (toGraph1 p)
 
@@ -30,12 +26,7 @@ intensionalize d = \p → (\q → (\w → (d (flip p $ w) (flip q $ w))))
 intensionalizeR ∷ ((Ent → T) → (Ent → T) → T) → ((Ent → T) → (Ent → (S → T)) → (S → T))
 intensionalizeR d = \p → (\q → (\w → (d p (flip q $ w))))
 
--- >>> toGraph1 $ (intensionalize each) _isAngry' _left'
--- [(W1,*** Exception: Model.hs:(75,1)-(77,21): Non-exhaustive patterns in function _isAngry'
-
 -- Now we can do de re with pronouns and otherwise everything automatically gets bound locally.
 
 -- >>> toGraph1 $ ( _believe' $ (intensionalizeR each) (_boy' <*> (pure W1)) (_left')) Tom
 -- [(W1,True),(W2,False),(W3,False)]
-
--- This makes interesting use of applicatives.
