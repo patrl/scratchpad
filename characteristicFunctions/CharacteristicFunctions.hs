ef2de5a789cb6aeb305b46339d38caf4d449f976
Patrick Elliott <patrick.d.elliott@gmail.com>
2018-05-11 14:02:53 +0200

    fixed characteristic functions

--- characteristicFunctions/CharacteristicFunctions.hs
+++ characteristicFunctions/CharacteristicFunctions.hs
@@ -8,14 +8,16 @@ import Control.Applicative
 
 toGraph1 f = [(x, (f x)) | x <- [(minBound) ..]]
 
-charFunc1 f = map fst (toGraph1 f)
+charFunc1 f = [x | (x,True) <- (toGraph1 f) ]
 
 toGraph2 f = [(x, (uncurry f $ x)) | x <- (liftA2 (,) [(minBound) ..] [(minBound) ..] )]
 
-charFunc2 f = map fst (toGraph2 f)
+charFunc2 f = [x | (x,True) <- (toGraph2 f) ]
 
 toGraph3 f = [(x, ((uncurry . uncurry) f $ x)) | x <- (liftA2 (,) (liftA2 (,) [(minBound) ..] [(minBound) ..]) [(minBound) ..])]
 
-charFunc3 f = map fst (toGraph3 f)
+charFunc3 f = [x | (x,True) <- (toGraph3 f) ]
 
 toGraph4 f = [(x, ((uncurry . uncurry . uncurry) f $ x)) | x <- (liftA2 (,) (liftA2 (,) (liftA2 (,) [(minBound) ..] [(minBound) ..]) [(minBound) ..]) [(minBound) ..])]
+
+charFunc4 f = [x | (x,True) <- (toGraph4 f) ]
