62fe82651d76d37d6f1a918810781e8acad8082f
Patrick Elliott <patrick.d.elliott@gmail.com>
2018-05-30 17:30:08 +0200

    everything basically works now

--- chierchia/Chierchia.hs
+++ chierchia/Chierchia.hs
@@ -1,17 +1,24 @@
+-- TODO: replicate Chierchia's system using the following type constructors:
+
+-- Chierchia's dynamic semantics via the Reader and Continuation monads + monad transformers
+
 {-# LANGUAGE LambdaCase #-}
 
 module Chierchia where
 
 import Data.Coerce
-import Data.Functor.Compose
 import Control.Monad (replicateM)
+import Control.Lens (set, element)
 import Control.Applicative (liftA2)
-import Control.Lens
-import Data.Monoid (All)
+import Control.Monad.Cont
+import Control.Monad.Reader
+import Control.Monad.Identity
 
--- Montague's intensional logic
+--
+-- Basic extensional system
+--
 
--- Model theory
+type G' = Reader (C E)
 
 data E = A | B | C deriving (Eq, Show, Enum)
 
@@ -20,11 +27,59 @@ dom = [A .. C]
 
 type T = Bool
 
-instance Monoid Bool where
-  mempty = True
-  p `mappend` q = p && q
+boy :: E -> T
+boy A = True
+boy _ = False
 
-data S = W0 | W1 | W2 deriving (Eq, Show, Enum)
+girl :: E-> T
+girl A = False
+girl _ = True
+
+leave :: E -> T
+leave C = False
+leave _ = True
+
+--
+-- Generalized conjunction via the continuation monad.
+--
+
+-- using the continuation monad, we characterize the programs terminating in T values.
+type BoolT = ContT T Identity
+
+-- generalized conjunction conjoins two programs that terminate in a boolean type.
+andCT :: Cont T a -> Cont T a -> Cont T a
+andCT cf cg = ContT $ \k ->
+  Identity $ runCont cf $ \f ->
+  runCont cg $ \g ->
+  (runIdentity $ k f) && (runIdentity $ k g)
+
+negCT :: Cont T a -> Cont T a
+negCT cp = ContT $ \k ->
+  Identity $ runCont cp $ \p ->
+  not (runIdentity $ k p)
+
+-- conjoins a program that terminates in a boolean type with a trivially continuationized truth proposition. This will come in handy later.
+andHelper :: BoolT a -> BoolT T -> BoolT a
+cf `andHelper` ct = ContT $ \k ->
+  Identity $ runCont cf $ \f ->
+  runCont ct $ \t ->
+  (runIdentity $ k f) && t
+
+-- lowers a trivially continuationized proposition
+lower :: Cont T T -> T
+lower cp = runCont cp id
+
+
+--
+-- Monadic context-sensitive system
+--
+
+-- -- The set of assignments.
+assignments = [ xs | xs <- (replicateM (length dom) dom) ]
+
+-- -- The characteristic function of the set of assignments.
+assignmentsF :: G BoolT T
+assignmentsF = ReaderT $ fmap return $ toCharFunc assignments
 
 -- Variables are just integers
 type Var = Int
@@ -32,167 +87,133 @@ type Var = Int
 -- Contexts (i.e. assignment functions) are treated as sequences and parameterised to a type.
 type C a = [a]
 
--- Type constructor for assignment-sensitivity.
-newtype G b a = G ((C b) -> a)
+-- Type constructor for assignment sensitive meanings, using the ReaderT constructor from mtl. G is a reader monad parameterised to assignments.
+type G = ReaderT (C E)
 
-fromG :: G b a -> ((C b) -> a)
-fromG = coerce
 
--- (Static) pronouns are functions from an index n, and a sequence as, to the nth element of as.
-pro :: Var -> G a a
-pro n = G (\as -> (as !! n))
 
--- Functor, applicative, and monad instances for context-sensitive meanings.
+-- Lexical entry for pronouns.
+pro :: Var -> G Identity E
+pro n = ReaderT (\as -> Identity (as !! n))
 
-instance Functor (G a) where
-  fmap f (G x) = G $ fmap f x
+lowerG :: C E -> G m a -> m a
+lowerG g = ($ g) . runReaderT
 
-instance Applicative (G a) where
-  pure x = G $ \c -> x
-  (G f) <*> (G x) = G $ f <*> x
+modify :: C E -> Var -> E -> C E
+modify g n x = set (element n) x g
 
-instance Monad (G a) where
+modified g n = [ modify g n x  | x <- dom ]
+
+-- Standard existential quantification
+ex :: Var -> G Identity T -> G Identity T
+ex n (ReaderT p)
+  = ReaderT (\g ->
+               Identity (not . null $ filter
+                         (\h -> runIdentity $ p h)
+                         (modified g n)))
+
+-- Continuationized existential quantification (will come in handy later)
+exCont :: Var -> G BoolT T -> G BoolT T
+exCont n (ReaderT p) =
+  ReaderT $ \g -> ContT $ \k ->
+  Identity (not . null $ filter (\h -> runCont (p h) $ \q ->
+                                    runIdentity $ k q) (modified g n))
+
+--
+-- Chierchia's dynamic semantics using these ingredients.
+--
+
+-- This is Chierchia's type for a Context Change Potential - It takes a set of contexts, and returns a set of contexts.
+newtype CCP a = CCP { runCCP :: ReaderT ((G BoolT T)) (G BoolT) a }
+
+-- Lowers CCPs to continuations.
+lowerCCP :: C E -> G BoolT T -> CCP a -> BoolT a
+lowerCCP g p = lowerG g
+               . ($ p)
+               . runReaderT
+               . runCCP
+
+instance Functor CCP where
+
+  fmap f x = CCP $ ReaderT $ \p ->
+    ReaderT $ \g ->
+    (fmap f (lowerCCP g p $ x)) `andHelper` (lowerG g $ p)
+
+instance Applicative CCP where
+
+  pure x = CCP $ ReaderT $ \(ReaderT p) ->
+    ReaderT $ \g ->
+    return x `andHelper` p g
+
+  f <*> x = CCP $ ReaderT $ \p ->
+    ReaderT $ \g ->
+    ((lowerCCP g p $ f) <*> (lowerCCP g p $ x))
+    `andHelper` (lowerG g $ p)
+
+instance Monad CCP where
+
   return = pure
-  (G x) >>= f = G (\c -> ((coerce (f (x c))) c))
 
--- Type constructor for discourse-marker sensitive meanings
-newtype Dm a = Dm ((C E) -> a)
+  x >>= f = CCP $ ReaderT $ \p ->
+    ReaderT $ \g ->
+    ((lowerCCP g p $ x) >>=
+    \y ->
+      lowerCCP g p $ f y) `andHelper` (lowerG g $ p)
 
-instance Functor Dm where
-  fmap f (Dm x) = Dm $ fmap f x
+-- function to lift from assignment-sensitive meanings to dynamic meanings.
+liftG :: G Identity a -> CCP a
+liftG (ReaderT x) = CCP $ ReaderT $ \(ReaderT p) ->
+  ReaderT $ \g ->
+  (return . runIdentity $ x g)
+  `andHelper` (p g)
 
-instance Applicative Dm where
-  pure x = Dm $ \c -> x
-  (Dm f) <*> (Dm x) = Dm $ f <*> x
 
-fromDm :: Dm a -> ((C E) -> a)
-fromDm = coerce
+-- Chierchia's dynamic existential quantification is also treated as a kind of function composition
+exCCP :: Var -> CCP T -> CCP T
+exCCP n (CCP (ReaderT scope))
+  = CCP $ ReaderT $ \p -> exCont n . scope $ p
 
--- >>> charFunc1 $ ((pure leaveExt) <*> (proDm 0)) `andDm` ((pure leaveExt) <*> (proDm 1))
--- [[A,A,A],[A,A,B],[A,A,C]]
+-- Chierchia's dynamic conjunction is a treated as a kind of function composition.
+andCCP :: CCP T -> CCP T -> CCP T
+(CCP (ReaderT c1)) `andCCP` (CCP (ReaderT c2))
+  = CCP $ ReaderT $ \p -> c1 . c2 $ p
 
-andDm :: Dm T -> Dm T -> Dm T
-andDm = liftA2 (&&)
+-- negCCP
+negCCP :: CCP a -> CCP a
+negCCP prej = CCP $ ReaderT $ \p ->
+  ReaderT $ \g ->
+  negCT (lowerCCP g p $ prej)
 
--- helper functions
-
-toSetOfSets :: ((E -> T) -> T) -> [[E]]
-toSetOfSets q = [ xs | xs <- (powerset dom), q (toCharFunc xs) ]
+-- Helper functions
 
 toCharFunc :: Eq a => [a] -> a -> T
-toCharFunc xs = (\x -> elem x xs)
+toCharFunc xs x = elem x xs
+
+toSetOfSets :: Eq a => [a] -> ((a -> T) -> T) -> [[a]]
+toSetOfSets qDom q = [ xs | xs <- (powerset qDom), q (toCharFunc xs) ]
 
 powerset :: [a] -> [[a]]
 powerset [] = [[]]
 powerset (x:xs) = powerset xs ++ map (x:) (powerset xs)
 
+toGraph :: [a] -> (a -> T) -> [(a,T)]
+toGraph dom f = [(x, (f x)) | x <- dom]
 
--- Discourse markers are functions from an index n, and a sequences of entities as, to the nth element of as.
-proDm :: Var -> Dm E
-proDm n = Dm (\as -> (as !! n))
+toSet :: [a] -> (a -> T) -> [a]
+toSet dom f = [x | (x,True) <-
+                   toGraph dom f]
 
--- Intensionality:
+prettyPrintAssignment :: C E -> IO ()
+prettyPrintAssignment = putStrLn
+                        . unwords
+                        . map show
 
-newtype Intension a = Intension (S -> a)
-
-fromInt :: Intension a -> (S -> a)
-fromInt = coerce
-
-instance Functor Intension where
-  fmap f (Intension a) = Intension $ fmap f a
-
-instance Applicative Intension where
-  pure a = Intension $ \w -> a
-  (Intension f) <*> (Intension x) = Intension $ f <*> x
-
-instance Monad Intension where
-  return = pure
-  (Intension a) >>= f = (Intension (\w -> ((coerce (f (a w))) w)))
-
-assignments = [ xs | xs <- (replicateM (length dom) dom) ]
-
-leaveInt :: E -> Intension T
-leaveInt = \case
-  A -> (Intension (\case
-           W0 -> True
-           W1 -> False
-           W2 -> True ))
-  B -> (Intension (\case
-           W0 -> True
-           W1 -> False
-           W2 -> False ))
-  C -> (Intension (\case
-           W0 -> True
-           W1 -> True
-           W2 -> False ))
-
-boyExt :: E -> T
-boyExt = \case
-  A -> True
-  B -> False
-  C -> False
-
-girlExt :: E-> T
-girlExt = \case
-  A -> False
-  _ -> True
-
-leaveExt :: E -> T
-leaveExt = \case
-  A -> True
-  B -> False
-  C -> False
-
-leaveDyn :: E -> Dm T -> Dm T
-leaveDyn x p = (liftA2 (&&) (pure (leaveExt x)) p)
-
-aExt :: (E -> T) -> (E -> T) -> T
-aExt p q = (not . null) [x | x <- dom, p x && q x]
-
--- aDyn :: (E -> Dm T -> Dm T) -> (E -> Dm T -> Dm T) -> Dm T -> Dm T
--- aDyn p q = \p -> 
-
-
-allAssignments :: C E -> T
-allAssignments = toCharFunc assignments
-
-onlyBAssignments = toCharFunc [[B,B,B]]
-
-newtype CCC a = CCC (Dm T -> Dm a)
-
-instance Functor CCC where
-  fmap f (CCC x) = CCC $ (fmap . fmap) f x
-
-instance Applicative CCC where
-  pure x = (CCC (\p -> ((Dm (\c -> x)))))
-  (<*>) = undefined
-
-
--- Showing how Chierchia's system builds on an ordinarily context-sensitive system.
--- >>> charFunc1 $ Dm ((\p -> (\c -> (((fromDm ((pure leaveExt) <*> (proDm 0))) c) && (p c)))) allAssignments)
--- [[A,A,A],[A,A,B],[A,A,C],[A,B,A],[A,B,B],[A,B,C],[A,C,A],[A,C,B],[A,C,C]]
-
-
-
--- >>> aExt girlExt leaveExt
--- False
-
-worlds :: [S]
-worlds = [W0 .. W2]
-
--- >>> toList ((pure leaveInt) <*> (pro 0))
--- [([A,A,A],W0),([A,A,A],W2),([A,A,B],W0),([A,A,B],W2),([A,A,C],W0),([A,A,C],W2),([A,B,A],W0),([A,B,A],W2),([A,B,B],W0),([A,B,B],W2),([A,B,C],W0),([A,B,C],W2),([A,C,A],W0),([A,C,A],W2),([A,C,B],W0),([A,C,B],W2),([A,C,C],W0),([A,C,C],W2),([B,A,A],W0),([B,A,B],W0),([B,A,C],W0),([B,B,A],W0),([B,B,B],W0),([B,B,C],W0),([B,C,A],W0),([B,C,B],W0),([B,C,C],W0),([C,A,A],W0),([C,A,A],W1),([C,A,B],W0),([C,A,B],W1),([C,A,C],W0),([C,A,C],W1),([C,B,A],W0),([C,B,A],W1),([C,B,B],W0),([C,B,B],W1),([C,B,C],W0),([C,B,C],W1),([C,C,A],W0),([C,C,A],W1),([C,C,B],W0),([C,C,B],W1),([C,C,C],W0),([C,C,C],W1)]
-
-
-toGraph (G f) = [(x, (uncurry (fmap fromInt f) $ x)) | x <- (liftA2 (,) assignments worlds )]
-
-toList p = [x | (x,True) <- (toGraph p) ]
-
-toGraphDm (Dm f) = [(x, (f x)) | x <- assignments]
-
-charFunc1 f = [x | (x,True) <- (toGraphDm f) ]
-
-toGraphCC (Dm f) = [(x, (uncurry (fmap fromDm f) $ x)) | x <- (liftA2 (,) assignments assignments)]
-
-
+showApGContext :: G BoolT T -> CCP T -> IO ()
+showApGContext context = mapM_ prettyPrintAssignment
+                 . toSet assignments
+                 . fmap lower
+                 . runReaderT
+                 . ($ context)
+                 . runReaderT
+                 . runCCP
 
