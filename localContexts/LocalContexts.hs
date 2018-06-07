dfecaed84c3afc984dc85db018d1d5e5c371ced0
Patrick Elliott <patrick.d.elliott@gmail.com>
2018-06-07 12:28:44 +0200

    exploring commonalities between chierchia and local contexts2

--- localContexts/LocalContexts.hs
+++ localContexts/LocalContexts.hs
@@ -5,7 +5,7 @@ module LocalContexts where
 
 import Control.Monad.Cont
 import Control.Monad.Identity
-import Control.Monad.State
+import Control.Monad.Reader
 
 -- replacing syntactic sugar with a function
 if' :: Bool -> a -> a -> a
@@ -28,16 +28,41 @@ andM cf cg = ContT $ \k ->
                       cg >>- \g ->
                                (runIdentity $ k f) && (runIdentity $ k g)
 
+-- intensional generalized conjunction
+andS :: Cont (S -> T) a -> Cont (S -> T) a -> Cont (S -> T) a
+andS cf cg = ContT $ \k ->
+  Identity $ cf >>- \f ->
+                      cg >>- \g ->
+                               (liftM2 (&&)) (runIdentity $ k f) (runIdentity $ k g)
+
+-- conjoin the terminating value of a predicate with the local context.
+andHelper :: Cont (S -> T) a -> (S -> T) -> Cont (S -> T) a
+andHelper cf p = ContT $ \k ->
+  Identity $ cf >>- \f ->
+                      (liftM2 (&&)) (runIdentity $ k f) p
+
+entailsHelper :: Cont (S -> T) (S -> T) -> Cont (S -> T) a -> Cont (S -> T) a
+entailsHelper cp cf = ContT $ \k ->
+  Identity $ cf >>- \f ->
+  cp >>- \p ->
+  (liftM2 (==>)) p (runIdentity $ k f)
+
+andHelper' :: Cont (S -> T) a -> Cont (S -> T) (S -> T) -> Cont (S -> T) a
+andHelper' cf cp = ContT $ \k ->
+  Identity $ cf >>- \f ->
+  cp >>- \p ->
+  (liftM2 (&&)) (runIdentity $ k f) p
+
 -- material implication
 (==>) :: T -> T -> T
 p ==> q = not p || q
 
 -- generalized entailment
-(|=) :: Cont T a -> Cont T a -> Cont T a
+(|=) :: Cont (S -> T) a -> Cont (S -> T) a -> Cont (S -> T) a
 cf |= cg = ContT $ \k ->
   Identity $ cf >>- \f ->
                       cg >>- \g ->
-                               (runIdentity $ k f) ==> (runIdentity $ k g)
+                               (liftM2 (==>)) (runIdentity $ k f) (runIdentity $ k g)
 
 type T = Bool
 
@@ -74,4 +99,73 @@ hugs A A = toCharFunc [0,1]
 hugs B B = toCharFunc [0,1]
 hugs C C = toCharFunc [0,1]
 hugs _ _ = toCharFunc [1]
+
+newtype LC a = LC { runLC :: Cont (S -> T) (S -> T) -> Cont (S -> T) a }
+
+universe :: Cont (S -> T) (S -> T)
+universe = (return :: a -> Cont (S -> T) a) $ toCharFunc [0..]
+
+-- More generally: A monad for checking that the value of the local environment is compatible with the terminating value of the computation. Almost identical to the Chierchia monad.
+
+instance Functor LC where
+  fmap f (LC a) = LC $ \p ->
+                         fmap f (a p)
+                         `andHelper'` p
+
+instance Applicative LC where
+  pure a = LC $ \p ->
+                  (pure a) `andHelper'` p
+  (LC f) <*> (LC x) = LC $ \p ->
+                             (f p) <*> (x p)
+                             `andHelper'` p
+
+instance Monad LC where
+  return = pure
+  (LC x) >>= f = LC $ \p ->
+    ((x p) >>= (\y ->
+                 ($ p) . runLC $ f y)) `andHelper'` p
+
+-- >>> toSet [0..10] . lower . ($ (return $ toCharFunc [0..10])) . runLC $ (return :: a -> LC a) ((liftM2 (&&)) ((return A) >>= leave) ((return A) >>= boy))
+-- [0,2,3,4]
+
+-- update
+(+++) :: LC (S -> T) -> LC (S -> T) -> LC (S -> T)
+(LC p) +++ (LC q) = LC $ \r ->
+                          p . q $ r
+
+-- >>> toSet [0..] . lower . ($ universe) . runLC $ (return ((return A) >>= leave)) +++ (return ((return A) >>= boy))
+-- [0,2,3,4]
+
+
+
+  -- let's try something presuppositional. leave presupposes now that A is a boy.
+leavePresupp :: LC (E -> S -> T)
+leavePresupp = LC $ \p ->
+                      ContT $ \k ->
+                                k (\x -> (leave x))
+
+  
+                      -- ifF (p `entailsHelper` (return $ boy)) (return leave) (return (\x -> (\w -> False)))
+
+-- >>> 1 + 1
+-- 2
+
+-- >>> toSet [0..10] . lower . ($ universe) . runLC $ leavePresupp `ap` (return A)
+-- >>> presupposition narrows down the context in exactly the way we predict.
+-- [0,2,3,4]
+ 
+-- lowers a continuationized proposition
+lower :: Cont a a -> a
+lower cp = runCont cp id
+
+toSet :: [a] -> (a -> T) -> [a]
+toSet dom f = [x | (x,True) <-
+                   toGraph dom f]
+
+-- a function from a predicate and a domain, to the graph of the predicate
+toGraph :: [a] -> (a -> T) -> [(a,T)]
+toGraph dom f = [(x, (f x)) | x <- dom]
+
+
+
 
