77b685b992eb8a63395c099cbc7c369e0924e109
Patrick Elliott <patrick.d.elliott@gmail.com>
2018-06-26 12:16:24 +0200

    linted code

--- localContexts/LocalContexts.hs
+++ localContexts/LocalContexts.hs
@@ -1,5 +1,6 @@
 -- looks like we need the State monad and the Cont T monad to reconstruct Schlenker's notion of local contexts. It's gonna look EXTREMELY similar to Chierchia's dynamic semantics.
 -- TODO: check out Woojin's paper: https://drive.google.com/file/d/0B4lu5n7x39bbOGpMeG5aMjBvc0E
+-- TODO: build the update logical directly into the semantics
 
 module LocalContexts where
 
@@ -9,20 +10,17 @@ import Control.Monad.Reader
 
 (>>-) = runCont
 
-toCharFunc :: Eq a => [a] -> a -> T
-toCharFunc xs x = elem x xs
-
 entailsHelper :: Cont (S -> T) (S -> T) -> Cont (S -> T) a -> Cont (S -> T) a
 entailsHelper cp cf = ContT $ \k ->
   Identity $ cf >>- \f ->
   cp >>- \p ->
-  (liftM2 (==>)) p (runIdentity $ k f)
+  liftM2 (==>) p (runIdentity $ k f)
 
 andHelper' :: Cont (S -> T) a -> Cont (S -> T) (S -> T) -> Cont (S -> T) a
 andHelper' cf cp = ContT $ \k ->
   Identity $ cf >>- \f ->
   cp >>- \p ->
-  (liftM2 (&&)) (runIdentity $ k f) p
+  liftM2 (&&) (runIdentity $ k f) p
 
 -- material implication
 (==>) :: T -> T -> T
@@ -33,15 +31,17 @@ p ==> q = not p || q
 cf |= cg = ContT $ \k ->
   Identity $ cf >>- \f ->
                       cg >>- \g ->
-                               (liftM2 (==>)) (runIdentity $ k f) (runIdentity $ k g)
+                               liftM2 (==>) (runIdentity $ k f) (runIdentity $ k g)
 
 type T = Bool
 
-data E = A | B | C deriving (Eq, Show, Enum)
-
 type S = Int
 
+data E = A | B | C deriving (Eq, Show, Enum)
+
 type I a = S -> a
+
+type Intension a = Reader S a
 
 contextSet = [0..]
 
@@ -61,9 +61,9 @@ girl C = toCharFunc [3,4,5,7]
 
 --
 leave :: E -> S -> T
-leave A = toCharFunc [0..7]
+leave A = toCharFunc [1,2,3,4,5,6,7]
 leave B = toCharFunc []
-leave C = toCharFunc [3,4,5,7]
+leave C = toCharFunc [0,4,5,7]
 
 hugs :: E -> E -> S -> T
 hugs A A = toCharFunc [0,1]
@@ -85,16 +85,15 @@ instance Functor LC where
 
 instance Applicative LC where
   pure a = LC $ \p ->
-                  (pure a) `andHelper'` p
+                  pure a `andHelper'` p
   (LC f) <*> (LC x) = LC $ \p ->
-                             (f p) <*> (x p)
+                             f p <*> x p
                              `andHelper'` p
 
 instance Monad LC where
   return = pure
   (LC x) >>= f = LC $ \p ->
-    ((x p) >>= (\y ->
-                 ($ p) . runLC $ f y)) `andHelper'` p
+    (x p >>= ($ p) . runLC . f) `andHelper'` p
 
 -- >>> toSet [1..10] (lower . ($ (return $ toCharFunc [0..10])) . runLC $ (return :: a -> LC a) ((liftM2 (&&)) ((return A) >>= leave) ((return A) >>= boy)))
 
@@ -103,16 +102,15 @@ instance Monad LC where
 (LC p) +++ (LC q) = LC $ \r ->
                           p . q $ r
 
+-- toy example of a presuppositional predicate.
 leavePresupp :: LC (E -> S -> T)
 leavePresupp = LC $ \r ->
   ContT $ \k ->
-            k (\x ->
-                 \s -> if null [w | w <- [0..10], (((lower r) w) && not (boy x w))] then leave x s else False)
+            k (\x s ->
+                 null [w | w <- [0..10], lower r w && not (boy x w)] && leave x s)
 
--- >>> toSet [1..10] (lower . ($ (return $ toCharFunc [2,3,4,5])) . runLC $ (leavePresupp `ap` (return A)))
--- []
+-- helper functions
 
--- lowers a continuationized proposition
 lower :: Cont a a -> a
 lower cp = runCont cp id
 
@@ -122,4 +120,7 @@ toSet dom f = [x | (x,True) <-
 
 -- a function from a predicate and a domain, to the graph of the predicate
 toGraph :: [a] -> (a -> T) -> [(a,T)]
-toGraph dom f = [(x, (f x)) | x <- dom]
+toGraph dom f = [(x, f x) | x <- dom]
+
+toCharFunc :: Eq a => [a] -> a -> T
+toCharFunc xs x = x `elem` xs
