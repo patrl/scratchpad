917ac91f359fd4c801a7a1f58a1749ac8f13d807
Patrick Elliott <patrick.d.elliott@gmail.com>
2018-07-02 10:26:03 +0200

    linted

--- charlow2018/Model.hs
+++ charlow2018/Model.hs
@@ -3,31 +3,36 @@
 module Model where
 
 -- The model:
-
 -- Variables
-data Var = Var_1 | Var_2 | Var_3 deriving (Eq, Show)
+data Var
+  = Var_1
+  | Var_2
+  | Var_3
+  deriving (Eq, Show)
 
-data Ent = Tom | Dick | Harry deriving (Eq, Show)
+data Ent
+  = Tom
+  | Dick
+  | Harry
+  deriving (Eq, Show)
 
 -- Truth values
 type T = Bool
 
 -- (type flexible) Assignments
-type Assignment a = Var → a
+type Assignment a = Var -> a
 
 -- One-place predicates
-
-_left ∷ Ent → T
+_left :: Ent -> T
 _left Tom = True
 _left _ = False
 
-_isAngry ∷ Ent → T
+_isAngry :: Ent -> T
 _isAngry Tom = False
 _isAngry _ = True
 
 -- Two-place predicates
-
-_likes ∷ Ent → Ent → T
+_likes :: Ent -> Ent -> T
 _likes Harry Tom = True
 _likes Tom Harry = True
 _likes Tom Tom = True
@@ -35,13 +40,10 @@ _likes Harry Harry = True
 _likes _ _ = False
 
 -- The brother function
-
-_brother ∷ Ent → Ent
+_brother :: Ent -> Ent
 _brother Tom = Harry
 _brother Harry = Tom
 
 -- Quantifiers
-
-_eachOfTomAndHarry ∷ (Ent → T) → T
+_eachOfTomAndHarry :: (Ent -> T) -> T
 _eachOfTomAndHarry f = f Tom && f Harry
-
---
+++ localContexts/ContExperimentLC.hs
@@ -0,0 +1,1 @@
+module ContExperimentLC where
