0e1d8a7855273c50c94fadf58c6b898e6f91c09d
Patrick Elliott <patrick.d.elliott@gmail.com>
2018-07-02 11:50:10 +0200

    added movement constructor

--- movement/Movement.hs
+++ movement/Movement.hs
@@ -1,6 +1,8 @@
 module Movement where
 
 import Data.Functor.Compose
+import Control.Monad (join, liftM, liftM2)
+import Control.Applicative (liftA2)
 
 data E
   = A
@@ -13,31 +15,36 @@ dom = [A .. C]
 
 type T = Bool
 
-boy :: Arg T
+boy :: Arg E T
 boy A = True
 boy _ = False
 
-girl :: Arg T
+girl :: Arg E T
 girl A = False
 girl _ = True
 
-leave :: Arg T
+leave :: Arg E T
 leave C = False
 leave _ = True
 
-friend :: Arg (Arg T)
-friend A B = True
-friend B A = True
+friend :: Arg E (Arg E T)
+friend _ A = True
 friend _ _ = False
 
 someone :: K T E
 someone = K $ \k -> any k dom
+
+who :: [E]
+who = dom
 
 everyone :: K T E
 everyone = K $ \k -> all k dom
 
 -- Let's role out own continuation monad with handy infix notation
 data K a b = K { (>>-) :: (b -> a) -> a }
+
+lower :: K a a -> a 
+lower kt = kt >>- id
 
 instance Functor (K a) where
   fmap f kx =  K $ \k ->
@@ -54,16 +61,52 @@ instance Applicative (K a) where
 instance Monad (K a) where
   return = pure
   kx >>= m = K $ \k ->
-                   kx >>- \x -> undefined
+                   kx >>- \x ->
+                            (m x) >>- \f ->
+                                        k f
                             
 
-type Arg = (->) E
+type Arg a = (->) a
 
-type M = Compose Arg (K T)
+type M a b = Compose (Arg a) (K b)
 
-trace :: Arg a -> M a
+type Move m a = Compose (Arg a) m 
+
+trace :: Arg E a -> Move (K b) E a
 trace f = Compose $ \x ->
                       K $ \k ->
                             k (f x)
 
-test = getCompose $ (trace friend) <*> (pure A)
+whTrace :: Arg E a -> Move ([]) E a
+whTrace f = Compose $ \x ->
+                        [f x]
+
+traceGeneral :: Applicative m => Arg b a -> Move m b a
+traceGeneral f = Compose $ \x ->
+                         pure (f x)
+
+-- Movement derivation:
+ -- >>> lower $ someone >>= (getCompose $ (trace friend) <*> (pure A))
+-- True
+
+-- Non-movement derivation:
+-- >>> lower $ ((pure friend) <*> someone) <*> (pure A)
+-- True
+
+-- Two things move
+-- deriving scope rigidity with movement.
+
+-- >>> lower $ lower <$> ((>>=) everyone) <$> (getCompose <$> (someone >>= (getCompose $ (pure trace) <*> (trace friend))))
+-- False
+
+-- >>> lower $ someone >>= (getCompose $ (liftA2 (&&)) ((trace friend) <*> (pure A)) ((trace friend) <*> (pure B)))
+-- False
+
+-- >>> who >>= (getCompose $ (whTrace friend) <*> (pure A))
+-- [True,True,True]
+
+-- wh-movement and in-situ
+
+-- >>>  lower $ lower <$> ((liftM2) (>>=)) (return someone) (getCompose <$> (liftM trace) ((pure friend) <*> someone))
+-- True
+
