e45deb470a8007bdea34560b3b5f38e7acff57a2
Patrick Elliott <patrick.d.elliott@gmail.com>
2018-07-16 18:30:27 +0200

    Prettify

--- movement/Movement.hs
+++ movement/Movement.hs
@@ -1,8 +1,6 @@
 module Movement where
 
 import Data.Functor.Compose
-import Control.Monad (join, liftM, liftM2)
-import Control.Applicative (liftA2)
 
 data E
   = A
@@ -32,7 +30,7 @@ friend _ A = True
 friend _ _ = False
 
 someone :: K T E
-someone = K $ \k -> any k dom
+someone = K $ \k -> any k dom
 
 who :: [E]
 who = dom
@@ -41,72 +39,51 @@ everyone :: K T E
 everyone = K $ \k -> all k dom
 
 -- Let's role out own continuation monad with handy infix notation
-data K a b = K { (>>-) :: (b -> a) -> a }
+newtype K a b = K
+  { (>>-) :: (b -> a) -> a }
 
-lower :: K a a -> a 
+lower :: K a a -> a
 lower kt = kt >>- id
 
 instance Functor (K a) where
-  fmap f kx =  K $ \k ->
-                     kx >>- \x ->
-                              k $ f x
+  fmap f kx = K $ \k -> kx >>- \x -> k $ f x
 
 instance Applicative (K a) where
-  pure x =  K $ \k -> k x
-  kf <*> kx = K $ \k ->
-                    kf >>- \f ->
-                             kx >>- \x ->
-                                      k $ f x
+  pure x = K $ \k -> k x
+  kf <*> kx = K $ \k -> kf >>- \f -> kx >>- \x -> k $ f x
 
 instance Monad (K a) where
   return = pure
-  kx >>= m = K $ \k ->
-                   kx >>- \x ->
-                            (m x) >>- \f ->
-                                        k f
-                            
+  kx >>= m = K $ \k -> kx >>- \x -> m x >>- \f -> k f
 
 type Arg a = (->) a
 
 type M a b = Compose (Arg a) (K b)
 
-type Move m a = Compose (Arg a) m 
+type Move m a = Compose (Arg a) m
 
 trace :: Arg E a -> Move (K b) E a
-trace f = Compose $ \x ->
-                      K $ \k ->
-                            k (f x)
+trace f = Compose $ \x -> K $ \k -> k (f x)
 
-whTrace :: Arg E a -> Move ([]) E a
-whTrace f = Compose $ \x ->
-                        [f x]
+whTrace :: Arg E a -> Move [] E a
+whTrace f = Compose $ \x -> [f x]
 
 traceGeneral :: Applicative m => Arg b a -> Move m b a
-traceGeneral f = Compose $ \x ->
-                         pure (f x)
-
+traceGeneral f = Compose $ \x -> pure (f x)
 -- Movement derivation:
  -- >>> lower $ someone >>= (getCompose $ (trace friend) <*> (pure A))
 -- True
-
 -- Non-movement derivation:
 -- >>> lower $ ((pure friend) <*> someone) <*> (pure A)
 -- True
-
 -- Two things move
 -- deriving scope rigidity with movement.
-
 -- >>> lower $ lower <$> ((>>=) everyone) <$> (getCompose <$> (someone >>= (getCompose $ (pure trace) <*> (trace friend))))
 -- False
-
 -- >>> lower $ someone >>= (getCompose $ (liftA2 (&&)) ((trace friend) <*> (pure A)) ((trace friend) <*> (pure B)))
 -- False
-
 -- >>> who >>= (getCompose $ (whTrace friend) <*> (pure A))
 -- [True,True,True]
-
 -- wh-movement and in-situ
-
 -- >>>  lower $ lower <$> ((liftM2) (>>=)) (return someone) (getCompose <$> (liftM trace) ((pure friend) <*> someone))
 -- True
-
