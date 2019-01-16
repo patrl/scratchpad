359010ec7addbbbaaef497635fa350ae1f9734c3
Patrick Elliott <patrick.d.elliott@gmail.com>
2019-01-16 16:49:31 +0100

    moved update stuff out into its own repo

--- update/default.nix
+++
@@ -1,8 +0,0 @@
-{ mkDerivation, base, containers, mtl, stdenv }:
-mkDerivation {
-  pname = "update";
-  version = "0.1.0.0";
-  src = ./.;
-  libraryHaskellDepends = [ base containers mtl ];
-  license = stdenv.lib.licenses.bsd3;
-}
